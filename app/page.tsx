a5479dd9b5346bfc630d96c8f4ab8db41ccb88ff
fauzimuh258-ui <fauzimuh258@gmail.com>
2026-08-02 08:03:39 +0700

    Add Dashboard component to Home page

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Dashboard from "@/components/dashboard";
+
+export default function Home() {
+  return (
+    <main className="min-h-[calc(100vh-3.5rem)] bg-white dark:bg-slate-950">
+      <Dashboard />
+    </main>
+  );
+}
